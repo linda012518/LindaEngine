398bbb04dc2176bec532261facc465f21ffd74c8
linda <895613747@qq.com>
2025-12-04 19:03:05 +0800

    bugfix

--- LindaEngine/Editor/ImGui/src/PlayViewPanelEditor.cpp
+++ LindaEngine/Editor/ImGui/src/PlayViewPanelEditor.cpp
@@ -23,7 +23,7 @@ void PlayViewPanelEditor::OnImGuiRender()
 	uint64_t textureID = _renderTexture->nativeIDs[0];
 	ImGui::Image(reinterpret_cast<void*>(textureID), viewportPanelSize, ImVec2(0, 1), ImVec2(1, 0));
 
-	if (_renderTexture->width != viewportPanelSize.x || _renderTexture->height != viewportPanelSize.y)
+	if (viewportPanelSize.x > 0 && viewportPanelSize.y > 0 && (_renderTexture->width != viewportPanelSize.x || _renderTexture->height != viewportPanelSize.y))
 	{
 		RenderTextureManager::DeleteImmediately(_renderTexture);
 		_renderTexture = RenderTextureManager::Get((int)viewportPanelSize.x, (int)viewportPanelSize.y, _fboSpec);
