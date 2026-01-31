bebce6639deedc820c0c6640a88fbae5a92e7fb6
linda <895613747@qq.com>
2026-01-31 19:23:42 +0800

    增加ImGuizmo库，imgui增加宏定义IMGUI_DEFINE_MATH_OPERATORS

--- LindaEngine/External/imgui/imgui/imgui.h
+++ LindaEngine/External/imgui/imgui/imgui.h
@@ -62,6 +62,8 @@ Index of this file:
 
 #pragma once
 
+#define IMGUI_DEFINE_MATH_OPERATORS
+
 // Configuration file with compile-time options
 // (edit imconfig.h or '#define IMGUI_USER_CONFIG "myfilename.h" from your build system)
 #ifdef IMGUI_USER_CONFIG
