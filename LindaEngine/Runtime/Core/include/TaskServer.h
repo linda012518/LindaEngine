c492d686d5f4bf412f40012c724e05067ed0c448
linda <895613747@qq.com>
2026-03-13 19:48:46 +0800

    增加程序化天空和任务类

---
+++ LindaEngine/Runtime/Core/include/TaskServer.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include <thread>
+#include <mutex>
+#include <list>
+#include <functional>
+
+#include "LThread.h"
+
+namespace LindaEngine
+{
+	class TaskServer
+	{
+		typedef std::function<void()> Task;
+
+		struct TaskItem
+		{
+			Task task;
+			std::function<void()> callback;
+		};
+
+	public:
+		void AddTask(Task task, std::function<void()> callback = nullptr);
+		void Start();
+		void Close();
+
+	protected:
+		void OnRun(LThread* pThread);
+
+	private:
+		//任务数据
+		std::list<TaskItem> _tasks;
+		//任务数据缓冲区
+		std::list<TaskItem> _tasksBuf;
+		//改变数据缓冲区时需要加锁
+		std::mutex _mutex;
+		LThread _thread;
+
+	};
+}
---
+++ LindaEngine/Runtime/Core/src/TaskServer.cpp
@@ -0,0 +1,64 @@
+﻿#include "TaskServer.h"
+
+using namespace LindaEngine;
+
+void TaskServer::AddTask(Task task, std::function<void()> callback)
+{
+	std::lock_guard<std::mutex> lock(_mutex);
+	_tasksBuf.push_back({ std::move(task), std::move(callback) });
+}
+
+void TaskServer::Start()
+{
+	_thread.Start(nullptr, [this](LThread* pThread) {
+		OnRun(pThread);
+		}, nullptr);
+}
+
+void TaskServer::Close()
+{
+	_thread.Close();
+}
+
+void TaskServer::OnRun(LThread* pThread)
+{
+	while (pThread->IsRun())
+	{
+		//从缓冲区取出数据
+		if (!_tasksBuf.empty())
+		{
+			std::lock_guard<std::mutex> lock(_mutex);
+			for (auto item : _tasksBuf)
+			{
+				_tasks.push_back(std::move(item));
+			}
+			_tasksBuf.clear();
+		}
+		//如果没有任务
+		if (_tasks.empty())
+		{
+			std::chrono::milliseconds t(1);
+			std::this_thread::sleep_for(t);
+			continue;
+		}
+		//处理任务
+		for (auto item : _tasks)
+		{
+			if (item.task)
+				item.task();
+			if (item.callback)
+				item.callback();
+		}
+		_tasks.clear();
+	}
+	//程序要退出了，看看缓冲队列里有没有，有的话再处理了
+	for (auto item : _tasksBuf)
+	{
+		if (item.task)
+			item.task();
+		if (item.callback)
+			item.callback();
+	}
+}
+
+
